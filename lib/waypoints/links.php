7fdf6ca6a88037d0c269ae67bab79203bbc64e22
sebastianRavenna <sebastian.ravenna@gmail.com>
2024-09-10 18:28:19 -0300

    Reiniciando la subida, sin archivos grandes

---
+++ lib/waypoints/links.php
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+<?php
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
+=======
+<?php
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
+>>>>>>> 3762b2e7333a03ae982e1a6669248aafe6f9bde5
